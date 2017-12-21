770817bc091515e53a92837ecc602566c550027c
Your Name <hans@hanken.org>
2017-12-21 21:31:50 +0000

    Stuck

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if !current_user_enrolled_in?(@course)
+      redirect_to courses_path, alert: 'Enroll before you can access this page'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
